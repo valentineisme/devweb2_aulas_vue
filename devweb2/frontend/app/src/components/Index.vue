8f6df8adb611f446f6957c39908096e11016a99b
Brenon Paul <bp.brenon@gmail.com>
2019-08-29 10:03:33 -0300

    2

---
+++ devweb2/frontend/app/src/components/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class='container'>
+        <h1>Pagina Inicial</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Index',
+};
+</script>
---
+++ devweb2/frontend/app/src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-container grid-list-md>
+        <v-layout row wrap align-center justify-center fill-height>
+            <v-flex xs12 sm8 lg4 md5>
+                <v-card>
+                    <v-card-title>
+                        <span>
+                            Autenticação
+                        </span>
+                    </v-card-title>
+                    <v-spacer></v-spacer>
+                    <v-card-text>
+                        <v-form ref='form'>
+                            <v-container>
+                                <v-text-field
+                                    :counter="70"
+                                    label="usuario"
+                                    maxlength="70"
+                                    required
+                                />
+
+                                <v-text-field
+                                    :type:"password"
+                                    :counter="20"
+                                    label="senha"
+                                    maxlength="20"
+                                    required
+                                />
+                            </v-container>
+                            <v-btn class='green'>Login</v-btn>
+                        </v-form>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default{
+        name: 'Login',
+    }
+</script>
